da653aee2049cf403984f73b8bed577126b1b3dc
Sander Roosendaal <roosendaalsander@gmail.com>
2023-01-26 12:31:14 +0100

    running with flag, added WorkoutForm

--- go.mod
+++ go.mod
@@ -8,9 +8,19 @@ require (
 )
 
 require (
+	github.com/apache/thrift v0.13.1-0.20201008052519-daf620915714 // indirect
+	github.com/cnkei/gospline v0.0.0-20191204072713-842a72f86331 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.10.5 // indirect
+	github.com/sanderroosendaal/gorow v0.0.0-20230117210112-c10fe60de2a3 // indirect
+	github.com/sgreben/piecewiselinear v1.1.1 // indirect
 	github.com/smartystreets/goconvey v1.7.2 // indirect
+	github.com/xitongsys/parquet-go v1.6.0 // indirect
+	github.com/xitongsys/parquet-go-source v0.0.0-20201108113611-f372b7d813be // indirect
 	golang.org/x/net v0.5.0 // indirect
+	gonum.org/v1/gonum v0.9.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"context"
 	"encoding/json"
+	"os"
+	"io"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
+	"github.com/sanderroosendaal/gorow"
 
 	"golang.org/x/oauth2"
 	"moul.io/http2curl"
@@ -16,11 +21,7 @@ import (
 var (
 	config = oauth2.Config{
 		ClientID: "YCQ1hVjdRECtin2NN75HHPjlni63uAHUWbgNEcGT", // local
-		//ClientID: "d3EXkobGFIdWOWkPwgXyth8So3CdVTEQLZGVw8qh", // dev
-		//ClientID: "SpecU3qiWht3mVk5VAXYPsaryW1PnsyuAecCTWcZ", // prod
 		ClientSecret: "LKiXu7dL1biHtM84kMtYZnGbcbokWJVyA0tVPc9laRWJjpzFaNwTZmYruN7iGH6wVYC1kiH9HbxekWGL59XLBY3AVew5R3xNKgPfwq8G1LNglrriyBRPZXKFQTdjxOCx", // local
-		//ClientSecret: "wCg2brL8wDXhXcUTRDqoWm2h4naw8WORNUDHHO5dQ1AqBR62w8qsCmYrucMJHGWJ36OXJZqhZwyAy4to92ACdlNo6jayipwG98eP5hdsc213zPSleJgOFilYAECvtjE2", // dev
-		//ClientSecret: "Oh9lUhw5YeTekFy5fYT4Qj2Je4qqBcduMzvOQgA2JWlJw3Pom575KmFGPR6GOdf43vxRmOgdxSPuzqS9N2XMvNVyacgQjtHULsf96t3ouqkacIBlZGPT8jh1pA5ZSV1M", // prod
 		// Scopes:       []string{"read,write"},
 		RedirectURL: "http://localhost:9094/oauth2",
 		// This points to our Authorization Server
@@ -29,10 +30,32 @@ var (
 		Endpoint: oauth2.Endpoint{
 			AuthURL:  "http://localhost:8000/rowers/o/authorize/",
 			TokenURL: "http://localhost:8000/rowers/o/token/",
-			//AuthURL:  "https://dev.rowsandall.com/rowers/o/authorize/",
-			//TokenURL: "https://dev.rowsandall.com/rowers/o/token/",
-			//AuthURL:  "https://rowsandall.com/rowers/o/authorize/",
-			//TokenURL: "https://rowsandall.com/rowers/o/token/",
+		},
+	}
+	config_dev = oauth2.Config{
+		ClientID: "d3EXkobGFIdWOWkPwgXyth8So3CdVTEQLZGVw8qh", // dev
+		ClientSecret: "wCg2brL8wDXhXcUTRDqoWm2h4naw8WORNUDHHO5dQ1AqBR62w8qsCmYrucMJHGWJ36OXJZqhZwyAy4to92ACdlNo6jayipwG98eP5hdsc213zPSleJgOFilYAECvtjE2", // dev
+		// Scopes:       []string{"read,write"},
+		RedirectURL: "http://localhost:9094/oauth2",
+		// This points to our Authorization Server
+		// if our Client ID and Client Secret are valid
+		// it will attempt to authorize our user
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://dev.rowsandall.com/rowers/o/authorize/",
+			TokenURL: "https://dev.rowsandall.com/rowers/o/token/",
+		},
+	}
+	config_prod = oauth2.Config{
+		ClientID: "SpecU3qiWht3mVk5VAXYPsaryW1PnsyuAecCTWcZ", // prod
+		ClientSecret: "Oh9lUhw5YeTekFy5fYT4Qj2Je4qqBcduMzvOQgA2JWlJw3Pom575KmFGPR6GOdf43vxRmOgdxSPuzqS9N2XMvNVyacgQjtHULsf96t3ouqkacIBlZGPT8jh1pA5ZSV1M", // prod
+		// Scopes:       []string{"read,write"},
+		RedirectURL: "http://localhost:9094/oauth2",
+		// This points to our Authorization Server
+		// if our Client ID and Client Secret are valid
+		// it will attempt to authorize our user
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://rowsandall.com/rowers/o/authorize/",
+			TokenURL: "https://rowsandall.com/rowers/o/token/",
 		},
 	}
 )
@@ -50,18 +73,21 @@ type WorkoutBody struct {
 	Duration    string `json:"duration"`
 }
 
+// StrokeRecord defines json for strokes data
+type StrokeRecord struct {
+	Distance  float64 `json:"distance"`
+	Power     float64 `json:"power"`
+	Heartrate int32   `json:"hr"`
+	Pace      float64 `json:"pace"`
+	Time      float64 `json:"time"`
+	Spm       float64 `json:"spm"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
 // Strokes defines json for strokes data
 type Strokes struct {
-	Data []struct {
-		Distance  float64 `json:"distance"`
-		Power     float64 `json:"power"`
-		Heartrate int32   `json:"hr"`
-		Pace      float64 `json:"pace"`
-		Time      float64 `json:"time"`
-		Spm       float64 `json:"spm"`
-		Latitude  float64 `json:"latitude"`
-		Longitude float64 `json:"longitude"`
-	} `json:"data"`
+	Data []StrokeRecord `json:"data"`
 }
 
 // Workout for API v3
@@ -81,21 +107,37 @@ type Workout struct {
 
 // HomePage serves home page
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Homepage Hit!")
+	if verbose{
+		log.Println("Homepage Hit!")
+	}
 	u := config.AuthCodeURL("xyz")
+	if instance == "dev" {
+		u = config_dev.AuthCodeURL("xyz")
+	}
+	if instance == "prod" {
+		u = config_prod.AuthCodeURL("xyz")
+	} 
 	http.Redirect(w, r, u, http.StatusFound)
 }
 
 // Workouts get workouts
 func Workouts(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Requesting Workouts")
+	if verbose {
+		log.Println("Requesting Workouts")
+	}
 	url := "http://localhost:8000/rowers/api/workouts/"
-	//url := "https://dev.rowsandall.com/rowers/api/workouts/"
-	//url := "https://rowsandall.com/rowers/api/workouts/"
+	if instance == "dev" {
+		url = "https://dev.rowsandall.com/rowers/api/workouts/"		
+	}
+	if instance == "prod" {
+		url = "https://rowsandall.com/rowers/api/workouts/"
+	}
+
 	// Create a Bearer string by appending string access token
 	var bearer = fmt.Sprintf("Bearer %s", stoken)
-	log.Println(bearer)
-
+	if verbose {
+		log.Println(bearer)
+	}
 	// Create a new request using http
 	req, err := http.NewRequest("GET", url, nil)
 
@@ -108,7 +150,9 @@ func Workouts(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println("Error on response.\n[ERRO] -", err)
 	}
-	fmt.Printf("Response code %v\n", resp.StatusCode)
+	if verbose{
+		fmt.Printf("Response code %v\n", resp.StatusCode)
+	}
 
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Fprintf(w, "%s", body)
@@ -117,8 +161,12 @@ func Workouts(w http.ResponseWriter, r *http.Request) {
 // StrokeData v3
 func StrokeDataV3(w http.ResponseWriter, r *http.Request) {
 	url := "http://localhost:8000/rowers/api/v3/workouts/"
-	// url := "https://dev.rowsandall.com/rowers/api/v3/workouts/"
-	// url := "https://rowsandall.com/rowers/api/v3/workouts/"
+	if instance == "dev" {
+		url = "https://dev.rowsandall.com/rowers/api/v3/workouts/"		
+	}
+	if instance == "prod" {
+		url = "https://rowsandall.com/rowers/api/v3/workouts/"
+	}
 
 	var bearer = fmt.Sprintf("Bearer %s", stoken)
 
@@ -128,7 +176,9 @@ func StrokeDataV3(w http.ResponseWriter, r *http.Request) {
 	_ = json.Unmarshal([]byte(file), &data)
 
 	bodyjson, _ := json.Marshal(data)
-	fmt.Printf("%s\n", []byte(bodyjson))
+	if verbose {
+		fmt.Printf("%s\n", []byte(bodyjson))
+	}
 
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer([]byte(bodyjson)))
 	// req, _ := http.NewRequest("POST", url, bytes.NewBuffer(file))
@@ -136,14 +186,18 @@ func StrokeDataV3(w http.ResponseWriter, r *http.Request) {
 	req.Header.Add("Content-Type", "application/json")
 
 	command, _ := http2curl.GetCurlCommand(req)
-	fmt.Println(command)
+	if verbose {
+		fmt.Println(command)
+	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Println("Error on response. \n[ERRO] -", err)
 	}
-	fmt.Printf("Response code %v\n", resp.StatusCode)
+	if verbose {
+		fmt.Printf("Response code %v\n", resp.StatusCode)
+	}
 
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Fprintf(w, "%s", body)
@@ -153,10 +207,16 @@ func StrokeDataV3(w http.ResponseWriter, r *http.Request) {
 func StrokeData(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	id := r.Form.Get("id")
-	fmt.Println(id)
+	if verbose {
+		fmt.Println(id)
+	}
 	url := fmt.Sprintf("http://localhost:8000/rowers/api/v2/workouts/%s/strokedata/", id)
-	// url := fmt.Sprintf("https://dev.rowsandall.com/rowers/api/v2/workouts/%s/strokedata/", id)
-	//url := fmt.Sprintf("https://rowsandall.com/rowers/api/v2/workouts/%s/strokedata/", id)
+	if instance == "dev" {
+		url = fmt.Sprintf("https://dev.rowsandall.com/rowers/api/v2/workouts/%s/strokedata/", id)
+	}
+	if instance == "prod" {
+		url = fmt.Sprintf("https://rowsandall.com/rowers/api/v2/workouts/%s/strokedata/", id)
+	}
 
 	var bearer = fmt.Sprintf("Bearer %s", stoken)
 
@@ -166,7 +226,9 @@ func StrokeData(w http.ResponseWriter, r *http.Request) {
 	_ = json.Unmarshal([]byte(file), &data)
 
 	bodyjson, _ := json.Marshal(data)
-	fmt.Printf("%s\n", []byte(bodyjson))
+	if verbose {
+		fmt.Printf("%s\n", []byte(bodyjson))
+	}
 
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer([]byte(bodyjson)))
 	// req, _ := http.NewRequest("POST", url, bytes.NewBuffer(file))
@@ -174,14 +236,18 @@ func StrokeData(w http.ResponseWriter, r *http.Request) {
 	req.Header.Add("Content-Type", "application/json")
 
 	command, _ := http2curl.GetCurlCommand(req)
-	fmt.Println(command)
+	if verbose {
+		fmt.Println(command)		
+	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Println("Error on response. \n[ERRO] -", err)
 	}
-	fmt.Printf("Response code %v\n", resp.StatusCode)
+	if verbose {
+		fmt.Printf("Response code %v\n", resp.StatusCode)
+	}
 
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Fprintf(w, "%s", body)
@@ -189,13 +255,23 @@ func StrokeData(w http.ResponseWriter, r *http.Request) {
 
 // AddWorkout post a workout
 func AddWorkout(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Posting a workout")
+	if verbose {
+		fmt.Println("Posting a workout")
+	}
 
 	url := "http://localhost:8000/rowers/api/workouts/"
-	//url := "https://dev.rowsandall.com/rowers/api/workouts/"
-	//url := "https://rowsandall.com/rowers/api/workouts/"
+	if instance == "dev" {
+		url = "https://dev.rowsandall.com/rowers/api/workouts/"		
+	}
+	if instance == "prod" {
+		url = "https://rowsandall.com/rowers/api/workouts/"
+	}
+
 	var bearer = fmt.Sprintf("Bearer %s", stoken)
-	log.Println(bearer)
+	if verbose {
+		log.Println(bearer)		
+	}
+
 
 	bodystruct := WorkoutBody{
 		Name:        "From Go Client",
@@ -207,25 +283,156 @@ func AddWorkout(w http.ResponseWriter, r *http.Request) {
 	}
 
 	bodyjson, _ := json.Marshal(bodystruct)
-	fmt.Printf("%s\n", []byte(bodyjson))
+	if verbose {
+		fmt.Printf("%s\n", []byte(bodyjson))
+	}
 
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer([]byte(bodyjson)))
 	req.Header.Set("Authorization", bearer)
 	req.Header.Add("Content-Type", "application/json")
 
 	command, _ := http2curl.GetCurlCommand(req)
-	fmt.Println(command)
+	if verbose {
+		fmt.Println(command)
+	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Println("Error on response. \n[ERRO] -", err)
 	}
-	fmt.Printf("Response code %v\n", resp.StatusCode)
+	if verbose {
+		fmt.Printf("Response code %v\n", resp.StatusCode)
+	}
 
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Fprintf(w, "%s", body)
 
+}
+
+func WorkoutForm(w http.ResponseWriter, r *http.Request) {
+	url := "http://localhost:8000/rowers/api/v3/workouts/"
+	if instance == "dev" {
+		url = "https://dev.rowsandall.com/rowers/api/v3/workouts/"		
+	}
+	if instance == "prod" {
+		url = "https://rowsandall.com/rowers/api/v3/workouts/"		
+	}
+
+
+	var bearer = fmt.Sprintf("Bearer %s", stoken)
+	switch r.Method {
+	case "GET": {
+		http.ServeFile(w, r, "static/form.html")
+	}
+	case "POST": {
+		if err := r.ParseMultipartForm(0); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		fmt.Fprintf(w, "Post from website! r.PostForm = %v\n", r.PostForm)
+		name := r.FormValue("name")
+		workouttype := string(r.FormValue("workouttype"))
+		boattype := string(r.FormValue("boattype"))
+		notes := string(r.FormValue("notes"))
+		file, handler, err := r.FormFile("myFile")
+		if err != nil {
+			fmt.Fprintf(w, "Error Retrieving the File err: %v", err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "Uploaded File: %+v\n", handler.Filename)
+		fmt.Fprintf(w, "File Size: %+v\n", handler.Size)
+		fmt.Fprintf(w, "MIME Header: %+v\n", handler.Header)
+		fmt.Fprintf(w, "Name = %s\n", name)
+		fmt.Fprintf(w, "Workout Type = %s\n", workouttype)
+		fmt.Fprintf(w, "Boat Type = %s\n", boattype)
+		fmt.Fprintf(w, "Notes = %s\n", notes)
+
+		// Create file
+		dst, err := os.Create(handler.Filename)
+		defer dst.Close()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Copy the uploaded file to the created file on the filesystem
+		if _, err := io.Copy(dst, file); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w, "Successfully Uploaded File\n")
+		
+		strokes, err := gorow.ReadCSV(handler.Filename)
+		if err != nil {
+			fmt.Fprintf(w, "Error Reading the CSV File err: %v", err)
+			return
+		}
+		// we're good
+		workout := Workout{}
+		workout.Name = name
+		workout.WorkoutType = workouttype
+		workout.BoatType = boattype
+		workout.Notes = notes
+		workout.StartDateTime = time.Now().Format(time.RFC3339)
+		workout.Distance = int64(strokes[len(strokes)-1].Distance)
+		workout.ElapsedTime = int64((strokes[len(strokes)-1].Timestamp-strokes[0].Timestamp)*1000.)
+		record := StrokeRecord{}
+		data := Strokes{}
+		for i := range strokes {
+			record.Distance = strokes[i].Distance
+			record.Power = strokes[i].Power
+			record.Heartrate = int32(strokes[i].Hr)
+			record.Pace = 1000.*strokes[i].Pace
+			record.Time = (strokes[i].Timestamp-strokes[0].Timestamp)*1000.
+			record.Spm = strokes[i].Spm
+			record.Latitude = strokes[i].Latitude
+			record.Longitude = strokes[i].Longitude
+			data.Data = append(data.Data,record)
+		}
+		workout.Strokes = data
+
+		bodyjson, _ := json.Marshal(workout)
+		if verbose {
+			fmt.Printf("%s\n", []byte(bodyjson))
+		}
+
+		req, _ := http.NewRequest("POST", url, bytes.NewBuffer([]byte(bodyjson)))
+		// req, _ := http.NewRequest("POST", url, bytes.NewBuffer(file))
+		req.Header.Set("Authorization", bearer)
+		req.Header.Add("Content-Type", "application/json")
+
+		command, _ := http2curl.GetCurlCommand(req)
+		if verbose {
+			fmt.Println(command)
+		}
+		
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Println("Error on response. \n[ERRO] -", err)
+		}
+		if verbose {
+			fmt.Printf("Response code %v\n", resp.StatusCode)
+		}
+		
+		body, _ := ioutil.ReadAll(resp.Body)
+
+		fmt.Fprintf(w, "%s", body)
+
+		// delete file
+		err = os.Remove(handler.Filename)
+		if err != nil{
+			fmt.Fprintf(w, "Error removing temp file: %v", err)
+			
+		}
+
+	}
+	default:
+		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+	}
 }
 
 // Authorize to do authorization
@@ -243,12 +450,21 @@ func Authorize(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	token, err := config.Exchange(context.Background(), code)
+	u_config := config
+	if instance == "dev" {
+		u_config = config_dev
+	}
+	if instance == "prod" {
+		u_config = config_prod
+	}
+	token, err := u_config.Exchange(context.Background(), code)
 	stoken = token.AccessToken
 	refreshtoken = token.RefreshToken
 
-	log.Println(stoken)
-	log.Println(refreshtoken)
+	if verbose {
+		log.Println(stoken)
+		log.Println(refreshtoken)		
+	}
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -259,9 +475,21 @@ func Authorize(w http.ResponseWriter, r *http.Request) {
 	e.Encode(*token)
 }
 
+var instance = "local"
+var verbose = false
+
 func main() {
 	stoken = "aap"
 	refreshtoken = "noot"
+	// flags
+	flag.BoolVar(&(verbose), "v", false, "use -v to set verbose")
+	flag.StringVar(&(instance), "i", "local", "use -i instance to set instance (local, dev, prod)")
+	flag.Parse()
+	if verbose {
+		log.Println(instance)
+		log.Println(time.Now().Format(time.RFC3339))		
+	}
+
 	// 1 - We attempt to hit our Homepage route
 	// if we attempt to hit this unauthenticated, it
 	// will automatically redirect to our Auth
@@ -281,6 +509,8 @@ func main() {
 
 	http.HandleFunc("/strokedatav3", StrokeDataV3)
 
+	http.HandleFunc("/form", WorkoutForm)
+
 	// 3 - We start up our Client on port 9094
 	log.Println("Client is running at 9094 port.")
 	log.Println("Endpoints")
@@ -290,5 +520,6 @@ func main() {
 	log.Println("/workout")
 	log.Println("/strokedata")
 	log.Println("/strokedatav3")
+	log.Println("/form")
 	log.Fatal(http.ListenAndServe(":9094", nil))
 }
